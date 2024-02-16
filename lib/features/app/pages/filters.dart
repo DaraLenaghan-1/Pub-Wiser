b6f92354816097c7333662982d05c5febf003ef6
Dara Lenaghan <g00385153@gmit.ie>
2024-02-16 15:27:32 +0000

    added more filter options

--- lib/features/app/pages/filters.dart
+++ lib/features/app/pages/filters.dart
@@ -1,5 +1,5 @@
-import 'package:first_app/features/app/pages/tabs.dart';
-import 'package:first_app/features/app/widgets/main_drawer.dart';
+//import 'package:first_app/features/app/pages/tabs.dart';
+//import 'package:first_app/features/app/widgets/main_drawer.dart';
 import 'package:flutter/material.dart';
 
 class FiltersPage extends StatefulWidget {
@@ -13,6 +13,9 @@ class FiltersPage extends StatefulWidget {
 
 class _FiltersPageState extends State<FiltersPage> {
   var _beerGarden = false; // _beerGardenFilterSet
+  var _draughtIPA = false; // _draughtIPAFilterSet
+  var _sportsBar = false; // _sportsBarFilterSet
+  var _traidBar = false; // _traidBarFilterSet
 
   @override
   Widget build(BuildContext context) {
@@ -20,15 +23,15 @@ class _FiltersPageState extends State<FiltersPage> {
       appBar: AppBar(
         title: const Text('Filters'),
       ),
-      drawer: MainDrawer(onSelectPage: (identifier) {
-        if (identifier == 'pubs') {
-          Navigator.of(context).push( // pushReplacement
-            MaterialPageRoute(
-              builder: (ctx) => const TabsPage(),
-            ),
-          );
-        }
-      }),
+      // drawer: MainDrawer(onSelectPage: (identifier) {
+      //   if (identifier == 'pubs') {
+      //     Navigator.of(context).push( // pushReplacement
+      //       MaterialPageRoute(
+      //         builder: (ctx) => const TabsPage(),
+      //       ),
+      //     );
+      //   }
+      // }),
       body: Column(children: [
         SwitchListTile(
           value: _beerGarden,
@@ -37,7 +40,7 @@ class _FiltersPageState extends State<FiltersPage> {
               _beerGarden = isChecked;
             });
           },
-          title: Text('Beer Garden',
+          title: Text('Beer Gardens', // Beer Garden
               style: Theme.of(context).textTheme.titleLarge!.copyWith(
                     color: Theme.of(context).colorScheme.onBackground,
                   )),
@@ -50,6 +53,66 @@ class _FiltersPageState extends State<FiltersPage> {
           activeColor: Theme.of(context)
               .colorScheme
               .tertiary, // Color of the switch when it is on
+          contentPadding: const EdgeInsets.only(left: 34, right: 22),
+        ),
+        SwitchListTile(
+          value: _draughtIPA,
+          onChanged: (isChecked) {
+            setState(() {
+              _draughtIPA = isChecked;
+            });
+          },
+          title: Text('IPA', // IPA
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  )),
+          subtitle: Text(
+            'Only show pubs with draught IPA',
+            style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+          activeColor: Theme.of(context).colorScheme.tertiary,
+          contentPadding: const EdgeInsets.only(left: 34, right: 22),
+        ),
+        SwitchListTile(
+          value: _sportsBar,
+          onChanged: (isChecked) {
+            setState(() {
+              _sportsBar = isChecked;
+            });
+          },
+          title: Text('Sports Bars', // sports bar
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  )),
+          subtitle: Text(
+            'Only show pubs with sports on display',
+            style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+          activeColor: Theme.of(context).colorScheme.tertiary,
+          contentPadding: const EdgeInsets.only(left: 34, right: 22),
+        ),
+        SwitchListTile(
+          value: _traidBar,
+          onChanged: (isChecked) {
+            setState(() {
+              _traidBar = isChecked;
+            });
+          },
+          title: Text('Traid Bars', // IPA
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  )),
+          subtitle: Text(
+            'Only show pubs with draught IPA',
+            style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+          activeColor: Theme.of(context).colorScheme.tertiary,
           contentPadding: const EdgeInsets.only(left: 34, right: 22),
         ),
       ]),
